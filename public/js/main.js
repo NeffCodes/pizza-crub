9c76b0c41073e2b1f6a4436a2c436e6522df34fa
James Neff <james.m.neff@gmail.com>
2022-06-23 14:56:26 -0400

    add vote tracking

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,6 @@
+//// Initialize storage for vote tracking ///////////
+if(!localStorage.getItem('votedToppings')) localStorage.setItem('votedToppings', '{}')
+
 const deleteButtons = document.querySelectorAll('.fa-trash')
 const upvotes = document.querySelectorAll('.fa-thumbs-up')
 const downvotes = document.querySelectorAll('.fa-thumbs-down')
@@ -11,8 +14,6 @@ Array.from(downvotes).forEach( downvote => {
   downvote.addEventListener('click', updateLikes)
   downvote.vote = 'down'
 })
-
-
 
 async function deleteTopping() {
   const deleteCode = window.prompt("Password required:")
@@ -39,23 +40,29 @@ async function deleteTopping() {
 async function updateLikes(event){
   const topping = this.parentNode.childNodes[1].innerText
   const count = Number(this.parentNode.childNodes[3].innerText)
-  console.log(topping, event.currentTarget.vote, count)
+  const votedToppings = JSON.parse(localStorage.getItem('votedToppings'))
 
-  try {
-    const response = await fetch('updateLike', {
-      method:'put',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        'topping': topping,
-        'likes': count,
-        '_vote': event.currentTarget.vote
+  if(votedToppings[topping]) {
+    alert('Sorry, looks like you already casted your vote for this topping')
+  } else {
+    votedToppings[topping] = true;
+    localStorage.setItem('votedToppings', JSON.stringify(votedToppings))
+    try {
+      const response = await fetch('updateLike', {
+        method:'put',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          'topping': topping,
+          'likes': count,
+          '_vote': event.currentTarget.vote
+        })
       })
-    })
-    const data = await response.json();
-    console.log(data)
-    location.reload()
-  }
-  catch(err){
-    console.error(error)
+      const data = await response.json();
+      console.log(data)
+      location.reload()
+    }
+    catch(err){
+      console.error(error)
+    }
   }
 }
